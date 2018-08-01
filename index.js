77454140242570ba90792157c8c044a2e3702bb1
codebyomar <omarmohammed580@gmail.com>
2018-08-01 23:12:57 +0100

    enable cors

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ const app        = require('express')(),
  // Connect to database
  new Database(process.env.DB_PORT, process.env.DB_HOST, process.env.DB_NAME)    
 
+ //CORS
+ app.use(function(req, res, next) {
+   res.header("Access-Control-Allow-Origin", "*");
+   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+   next();
+ });    
  //routes
  app.use('/transfer-go/v1/send/', routes.send)
  app.use('/transfer-go/v1/claim/', routes.claim)
